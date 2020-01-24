4944ac55e3fe28f8876aa7a84c73d1bad8e4e5d8
Creeperwillie <55281367+Creeperwillie@users.noreply.github.com>
2020-01-24 07:53:30 -0500

    Update myScripts.js

--- javascripttest/myScripts.js
+++ javascripttest/myScripts.js
@@ -1,7 +1,13 @@
 function myLoop(){
 var start = document.getElementById("startLoop").value;
 var end = document.getElementById("endLoop").value;
-for(var i = start; i <= end; i++)
+var count = document.getElementById("countLoop").value;
+  
+  start = Number(start);
+  end = Number(end);
+  count = Number(count);
+  
+for(var i = start; i <= end; i+= count)
   {
     document.write("<p>"+ i + "</p>")
   }
